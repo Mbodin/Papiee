70a63d2c117b6d27592bd45010764f90742a74b9
Nathan Coquerel <Nathan.Coquerel@univ-grenoble-alpes.fr>
2025-11-14 11:27:57 +0100

    💄 Add logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import icon from '$lib/assets/icon.svg';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Papiée</title>
 </svelte:head>
 
+<div class="relative flex min-h-20 w-full flex-row items-center bg-surface-500 p-2">
+	<img class="m-0 max-h-20 w-40 rounded-md bg-white" src={icon} alt="Logo" />
+</div>
 {@render children?.()}
