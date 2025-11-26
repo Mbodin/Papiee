edb20dd0c7a9c9d00322101c27aa2302fe74a0bc
Nathan Coquerel <Nathan.Coquerel@univ-grenoble-alpes.fr>
2025-11-26 14:19:51 +0100

    ✨Add a loading screen to wait for the worker initialization

--- src/lib/rocq/RocqProvider.svelte
+++ src/lib/rocq/RocqProvider.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
 	import { close, create, initialize, WORKER_CONTEXT, type RocqWorker } from '$lib/rocq/connection';
+	import { Loader } from '@lucide/svelte';
 	import { onMount, setContext, type Snippet } from 'svelte';
 
 	import * as proto from 'vscode-languageserver-protocol';
@@ -27,4 +28,23 @@
 	setContext(WORKER_CONTEXT, worker);
 </script>
 
-{@render children?.()}
+{#if worker.connection != null}
+	{@render children?.()}
+{:else}
+	<div
+		class="bg-surface-0 my-5 mt-5 flex h-full flex-col rounded-lg bg-white p-20 text-neutral-950"
+	>
+		<div
+			class="m-auto block max-w-md overflow-hidden card border-surface-900-100 preset-filled-surface-900-100 card-hover"
+		>
+			<article class="space-y-4 p-4">
+				<div>
+					<h2 class="h6 text-neutral-950">Loading ...</h2>
+					<h3 class="h3 text-neutral-950">The worker is loading</h3>
+				</div>
+
+				<Loader class="m-auto animate-spin text-primary-500" size="128px" />
+			</article>
+		</div>
+	</div>
+{/if}
