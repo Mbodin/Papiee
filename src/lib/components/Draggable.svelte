b471636094f7ba0efd273672cb5e23a16277f058
Nathan Coquerel <Nathan.Coquerel@univ-grenoble-alpes.fr>
2025-11-26 17:17:43 +0100

    🐛Have draggable div be consistent with window size

--- src/lib/components/Draggable.svelte
+++ src/lib/components/Draggable.svelte
@@ -5,6 +5,7 @@
 	let { children }: { children: Snippet<[]> } = $props();
 
 	let div: HTMLDivElement | undefined = $state(undefined);
+	let element_size = $state({ width: 0, height: 0 });
 
 	const RootAttachment: Attachment<HTMLDivElement> = (element) => {
 		div = element;
@@ -12,50 +13,61 @@
 		document.addEventListener('mousemove', drag);
 		document.addEventListener('mouseup', stopDrag);
 		document.addEventListener('scroll', onScroll);
+		window.addEventListener('resize', onResize);
+
+		onResize();
 		return () => {
 			element.remove();
 			document.removeEventListener('scroll', onScroll);
 			document.removeEventListener('mousemove', drag);
 			document.removeEventListener('mouseup', stopDrag);
+			window.removeEventListener('resize', onResize);
 		};
 	};
 
 	let scroll_offset = $state({ x: 0, y: 0 });
+	function onScroll() {
+		scroll_offset.y = document.documentElement.scrollTop;
+	}
+
+	let document_size = $state({ width: 0, height: 0 });
+	function onResize() {
+		console.log('HH');
+		document_size = {
+			width: document.body.clientWidth,
+			height: document.body.clientHeight
+		};
+	}
 
 	let dragging = $state(false);
 	let source = $state({ x: 0, y: 0 });
 	let initial = $state({ x: 0, y: 0 });
-	let drag_position = $state({ x: Infinity, y: 0 });
-	let width = $state(0);
-	let height = $state(0);
+	let drag_position = $state({ x: 1, y: 0 });
+
 	let inbounds_position = $derived.by(() => {
 		let { x, y } = drag_position;
 		x = Math.max(x, 0);
-		x = Math.min(x, document.body.clientWidth - width);
+		x = Math.min(x, 1 - element_size.width / document_size.width);
 
 		y = Math.max(y, 0);
-		y = Math.min(y, document.body.clientHeight - height);
+		y = Math.min(y, 1 - element_size.height / document_size.height);
 		return { x, y };
 	});
 
 	function startDrag(e: MouseEvent) {
 		source = {
-			x: e.clientX,
-			y: e.clientY
+			x: e.clientX / document_size.width,
+			y: e.clientY / document_size.height
 		};
 		initial = { x: inbounds_position.x, y: inbounds_position.y };
 		dragging = true;
 	}
 
-	function onScroll() {
-		scroll_offset.y = document.documentElement.scrollTop;
-	}
-
 	function drag(e: MouseEvent) {
 		if (!dragging) return;
 
-		let x = initial.x + e.clientX - source.x;
-		let y = initial.y + e.clientY - source.y;
+		let x = initial.x + e.clientX / document_size.width - source.x;
+		let y = initial.y + e.clientY / document_size.height - source.y;
 		drag_position = { x, y };
 	}
 
@@ -68,13 +80,13 @@
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <div
 	bind:this={div}
-	bind:clientWidth={width}
-	bind:clientHeight={height}
+	bind:clientWidth={element_size.width}
+	bind:clientHeight={element_size.height}
 	{@attach RootAttachment}
 	class:dragging
 	class="dragging absolute z-10 min-h-0 min-w-0 select-none"
 	onmousedown={startDrag}
-	style={`top: ${inbounds_position.y + scroll_offset.y}px; left: ${inbounds_position.x}px; transition: top 1s in`}
+	style={`top: ${inbounds_position.y * document_size.height + scroll_offset.y}px; left: ${inbounds_position.x * document_size.width}px; transition: top 1s in`}
 >
 	{@render children()}
 </div>
