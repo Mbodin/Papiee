dc49df56979681423c46d9f8c2ed464c07355466
Nathan Coquerel <Nathan.Coquerel@univ-grenoble-alpes.fr>
2025-12-05 11:02:08 +0100

    🍱 Separate icon from favicon and introduce a smaller part of the icon as the favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,7 @@
 
 <header class="sticky flex min-h-20 w-full flex-row items-center bg-surface-50-950 p-2">
 	<div class="flex flex-row items-center gap-1">
-		<img class="m-0 max-h-10 rounded-md" src={'assets/favicon.svg'} alt="Logo" />
+		<img class="m-0 max-h-10 rounded-md" src={'assets/icon.svg'} alt="Logo" />
 		<h1 class="h1">Papiée</h1>
 	</div>
 	<div class="flex-1"></div>
