4d8a799823afe2151c2d4bd4841bfc0c5d1a9df6
Nathan Coquerel <Nathan.Coquerel@univ-grenoble-alpes.fr>
2025-12-05 11:08:01 +0100

    🐛Fix assets path being relative

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,13 +6,13 @@
 </script>
 
 <svelte:head>
-	<link rel="icon" href={'assets/favicon.svg'} />
+	<link rel="icon" href={'/assets/favicon.svg'} />
 	<title>Papiée</title>
 </svelte:head>
 
 <header class="sticky flex min-h-20 w-full flex-row items-center bg-surface-50-950 p-2">
 	<div class="flex flex-row items-center gap-1">
-		<img class="m-0 max-h-10 rounded-md" src={'assets/icon.svg'} alt="Logo" />
+		<img class="m-0 max-h-10 rounded-md" src={'/assets/icon.svg'} alt="Logo" />
 		<h1 class="h1">Papiée</h1>
 	</div>
 	<div class="flex-1"></div>
