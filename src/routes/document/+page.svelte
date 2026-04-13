8ca1965e7b644764a681840b8c44e477f77a8b5b
Martin Bodin <martin.bodin@inria.fr>
2026-04-13 17:25:11 +0200

    J'avais oublié le \in \mathbb{N} dans la page de démo.

--- src/routes/document/+page.svelte
+++ src/routes/document/+page.svelte
@@ -22,9 +22,9 @@
 		let proof = "Montrons que $\\forall n \\in \\mathbb{N}, \\exists k \\in \\mathbb{N}, A_ n = 3 * k$.\n" ;
 		proof += "Soit $n \\in \\mathbb{N}$.\n" ;
 		proof += "On distingue les cas suivants.\n" ;
-		proof += "\t- Si $n \\bmod 3 = 0$. Il existe alors $k$ tel que $n = 3 * k$. Alors $A_ n = 3 * (k * (2 * n + 1) * (7 * n + 1))$.\n" ;
-		proof += "\t- Si $n \\bmod 3 = 1$. Il existe alors $k$ tel que $n = 3 * k + 1$. Alors $2 * n + 1 = 3 * (2 * k + 1)$. Alors $A_ n = 3 * (n * (2 * k + 1) * (7 * n + 1)).$\n" ;
-		proof += "\t- Si $n \\bmod 3 = 2$. Il existe alors $k$ tel que $n = 3 * k + 2$. Alors $7 * n + 1 = 3 * (7 * k + 5)$. Alors $A_ n = 3 * (n * (2 * n + 1) * (7 * k + 5)).$\n" ;
+		proof += "\t- Si $n \\bmod 3 = 0$. Il existe alors $k \\in \\mathbb{N}$ tel que $n = 3 * k$. Alors $A_ n = 3 * (k * (2 * n + 1) * (7 * n + 1))$.\n" ;
+		proof += "\t- Si $n \\bmod 3 = 1$. Il existe alors $k \\in \\mathbb{N}$ tel que $n = 3 * k + 1$. Alors $2 * n + 1 = 3 * (2 * k + 1)$. Alors $A_ n = 3 * (n * (2 * k + 1) * (7 * n + 1)).$\n" ;
+		proof += "\t- Si $n \\bmod 3 = 2$. Il existe alors $k \\in \\mathbb{N}$ tel que $n = 3 * k + 2$. Alors $7 * n + 1 = 3 * (7 * k + 5)$. Alors $A_ n = 3 * (n * (2 * n + 1) * (7 * k + 5)).$\n" ;
 		proof += "Ce qu'il fallait démontrer.\n" ;
 		node.children.cnl_proof.value = proof ;
 	});
